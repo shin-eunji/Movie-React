df3a4f484b5b80a43ba588a2c615e2c6bdfb9ff4
shineunji <dmswlckato@gmail.com>
2020-07-28 14:16:27 +0900

    props

--- movie_app_2020/src/App.js
+++ movie_app_2020/src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
 
+function Food({ name }) {
+  return <h1>I like {name}</h1>;
+}
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello!!!!</h1>
+      <h1>Hello</h1>
+      <Food name="potato" />
+      {/* <Food
+        fav="kimbob"
+        something={true}
+        papapapa={["hello", 1, 2, 3, 4, true]}
+      /> */}
     </div>
   );
 }
