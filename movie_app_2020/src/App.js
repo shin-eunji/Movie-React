781d9b9c61240b3a768cd26c392b98df48693ccb
shineunji <dmswlckato@gmail.com>
2020-07-28 16:33:14 +0900

    componentDidMount setTimeout

--- movie_app_2020/src/App.js
+++ movie_app_2020/src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 
 class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 3000);
+  }
   render() {
-    return <div></div>;
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading.." : "We are ready"}</div>;
   }
 }
 
