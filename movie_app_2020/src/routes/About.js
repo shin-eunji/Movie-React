f701bbd3568b642baf0953adbb6fe4197ec82bec
shineunji <dmswlckato@gmail.com>
2020-07-29 15:50:03 +0900

    docs: routes

---
+++ movie_app_2020/src/routes/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <span>About this page: I built it becase I love movies.</span>
+    </div>
+  );
+}
+
+export default About;
