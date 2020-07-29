4aae1137c72ed976e207db95d3671a8d54ac1a92
shineunji <dmswlckato@gmail.com>
2020-07-29 22:19:14 +0900

    docs: navigation style

--- movie_app_2020/src/App.js
+++ movie_app_2020/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { HashRouter, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+
 import Home from "./routes/Home";
 import About from "./routes/About";
 
 function App() {
   return (
     <HashRouter>
-      <Route path="/" component={Home} />
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
     </HashRouter>
   );
--- movie_app_2020/src/routes/About.js
+++ movie_app_2020/src/routes/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function About() {
   return (
-    <div>
+    <div className="about__container">
       <span>About this page: I built it becase I love movies.</span>
     </div>
   );
