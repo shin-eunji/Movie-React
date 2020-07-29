674c7f21bd1fb169e8c54f692acef7629f01b18d
shineunji <dmswlckato@gmail.com>
2020-07-29 12:54:53 +0900

    start

--- movie_app_2020/src/Movie.js
+++ movie_app_2020/src/Movie.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 import "./Movie.css";
 
-function Movie({ year, title, summary, poster }) {
+function Movie({ year, title, summary, poster, genres }) {
   return (
     <div class="movie">
       <img src={poster} alt={title} title={title} />
@@ -22,6 +22,7 @@ Movie.propTypes = {
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
